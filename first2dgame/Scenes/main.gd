63434d9d015730123e07fc37a985eda029123e7b
kyryl <kyryllsamborskyi@gmail.com>
2024-10-06 13:01:15 +0200

    game_test

--- first2dgame/Scenes/main.gd
+++ first2dgame/Scenes/main.gd
@@ -4,8 +4,20 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+@export var mob_scene: PackedScene
+var score
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game():
+	score=0
+	$Player.start($Marker2D.position)
+	$StartTimer.start()
